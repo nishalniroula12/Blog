ead97d847e9972cf1e63bd9c150316b662e90b59
Nishal Niroula <nishalniroula12@gmail.com>
2026-07-22 12:14:03 +0545

    fixing axios

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api =axios.create({
-    baseURL:import.meta.env.RENDER_URL,
+    baseURL:import.meta.env.VITE_RENDER_URL,
     withCredentials:true
 })
 export default api
